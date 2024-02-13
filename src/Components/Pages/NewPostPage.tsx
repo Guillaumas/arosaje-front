70c497aaafa7b68022c9ceb8de0740da13c4eb7c
Guillaumas <guillaume.gillet.pro@gmail.com>
2024-02-13 19:06:31 +0100

    a tester mais j'attends tieums

--- src/Components/Pages/NewPostPage.tsx
+++ src/Components/Pages/NewPostPage.tsx
@@ -87,20 +87,3 @@ const NewPost = ({ onClose }: { onClose?: () => void }) => {
 };
 
 export default NewPost;
-
-
-
-
-//todo affichage de la page de nouveau post
-//todo affichage d'un formulaire a remplir pour ajouter une plante/post avec les criteres suivants:
-//todo - nom de la plante (optionnel)
-//todo - espece de la plante (obligatoire)
-//todo - image de la plante (obligatoire) (au moins une image)
-//todo - une previsualisation de la plante
-//todo - date de plantation (optionnel)
-//todo - description (optionnel)
-//todo - bouton de validation
-//todo - bouton d'annulation
-//todo - bouton de vidage du formulaire
-//todo le formulaire doit etre valide pour etre envoyé
-//todo le formulaire doit etre en overlay au dessus de la page de profile
