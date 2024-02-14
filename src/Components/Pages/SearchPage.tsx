259d714aa3c94e51971fa326272d72afb01e6312
Guillaumas <guillaume.gillet.pro@gmail.com>
2024-02-14 20:57:28 +0100

    ajout du bouton de contact de botaniste + du choix de la plante pour que l'utilisateur non botaniste, lors du contact au botaniste transmette les infos de la plante

--- src/Components/Pages/SearchPage.tsx
+++ src/Components/Pages/SearchPage.tsx
@@ -56,11 +56,3 @@ const SearchPage = () => {
 };
 
 export default SearchPage;
-
-//todo page de recherche de plantes
-//todo 1 barre de recherche / filtres
-//todo 2 affichage des resultats de la recherche
-//todo 3 si l'utilisateur appuie sur un resultat, afficher la page du post
-//todo 4 si l'utilisateur appuie sur les commentaires d'un resultat, afficher la page des commentaires de ce post
-//todo 5 afficher la navbar (accueil, profil, messages, parametres, recherche)
-
