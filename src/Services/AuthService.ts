9a3becdab7e866ac5f7f7550ddcbea4f4cb66174
MatthieuDeroir <matthieu.deroir@epsi.fr>
2024-02-17 16:14:32 +0100

    add: AuthService

--- src/Services/AuthService.ts
+++ src/Services/AuthService.ts
@@ -1,8 +1,8 @@
 import fetchFromAPI from './ApiService';
 
 export const AuthService = {
-    async login(username: string, password: string): Promise<any> {
-        return fetchFromAPI('login', 'POST', { username, password });
+    async login(email: string, password: string): Promise<any> {
+        return fetchFromAPI('login', 'POST', { email, password });
     },
     async register(userData: any): Promise<any> {
         return fetchFromAPI('register', 'POST', userData);
