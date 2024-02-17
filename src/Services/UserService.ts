d8262c1251d617bbaa81776156a071b13ad3a9d5
Guillaumas <guillaume.gillet.pro@gmail.com>
2024-02-17 23:53:55 +0100

    UserService.ts add + postpage de faite

---
+++ src/Services/UserService.ts
@@ -0,0 +1,23 @@
+import fetchFromAPI from "./ApiService";
+
+export const UserService = {
+    async fetchUserById(id: number): Promise<any> {
+        return fetchFromAPI(`users/${id}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`API call failed: ${response.statusText}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data === null) {
+                    throw new Error('No data returned from API');
+                }
+                return data;
+            })
+            .catch(error => {
+                console.error(`Error fetching user with id ${id}:`, error);
+                return null;
+            });
+    }
+}
